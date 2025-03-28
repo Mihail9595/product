44c9aca99efd98086b0a86fcb6a493741f347e8c
Mihail9595 <mihailkokskokin@gmail.com>
2025-03-28 18:33:42 +0300

    2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 import style from "../styles/header.module.css"
 const Header = () => {
     return ( <div className={style.header}>
-        <NavLink to={"/"}>HOME</NavLink> 
+        <NavLink to={"/product"}>HOME</NavLink> 
         <NavLink to={"/liked"}>LIKED</NavLink> 
         <NavLink to={"/create-product"}>CREATE</NavLink> 
     </div> );
