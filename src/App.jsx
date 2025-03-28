8bc2dfeb5b9981f268501ff036fd53fd27a41609
Mihail9595 <mihailkokskokin@gmail.com>
2025-03-28 21:03:22 +0300

    19

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <div className="container">
       <Header />
       <Routes >
-        <Route path="/product" element={<Products />} />
+        <Route path="/product" index element={<Products />} />
         <Route path="/liked" element={<Liked />} />
         <Route path="/create-product" element={<Create />} />
         <Route path="/:id" element={<FullProduct />} />
